7d769bebea46b966a7db1d19ec8ee66434fa4341
Matt Eva <matteweva@gmail.com>
2021-11-09 09:59:19 -0800

    added sort functionality

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({displayData, sortBy}) {
+  const [selectedAttribute, setSelectedAttribute] = useState("no sort")
+  const pokeCardList = displayData.map(pokeCard => <PokemonCard pokeCard={pokeCard} key={pokeCard.id}/>)
+
   return (
-    <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
-    </Card.Group>
+    <div>
+
+      <select value={selectedAttribute} style={{marginBottom: "10px"}} 
+      onChange={(e) => {
+        sortBy(e);
+        setSelectedAttribute(e.target.value)}}
+      >
+          <option value="no sort">No Sort</option>
+          <option value="hp">HP</option>
+          <option value="name">Name</option>
+      </select>
+
+      <Card.Group itemsPerRow={6}>
+        {pokeCardList}
+      </Card.Group>
+
+    </div>
+   
   );
 }
 
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -3,17 +3,85 @@ import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
+import { useState, useEffect } from "react";
 
 function PokemonPage() {
+  const [unsortedPokeData, setUnsortedPokeData] = useState([])
+  const [pokeData, setPokeData] = useState([])
+  const [search, setSearch] = useState("") 
+
+  const displayData = pokeData.filter(poke => poke.name.toLowerCase().includes(search.toLowerCase()))
+
+  useEffect(()=>{
+    fetch(`http://localhost:3001/pokemon`)
+      .then(r => r.json())
+      .catch(error => console.error(error))
+      .then(data => {
+        setPokeData([...data])
+        setUnsortedPokeData([...data])
+      })
+  }, [])
+
+  const searchInput = (input) =>{
+    setSearch(input)
+  }
+
+  const addPoke = (newPoke) => {
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newPoke)
+    }
+    fetch("http://localhost:3001/pokemon", configObj)
+    .then(r => r.json())
+    .catch(error => console.error(error))
+    .then(poke => setPokeData([...pokeData, poke]))
+  }
+
+  const sortBy = (e) =>{
+    if (e.target.value === "no sort"){
+      console.log(e.target.value)
+      setPokeData([...unsortedPokeData])
+    } else if (e.target.value === "hp"){
+      console.log(e.target.value)
+      const hpData = pokeData.sort((a, b) =>{
+        return b.hp - a.hp;
+      })
+      setPokeData([...hpData])
+    } else if (e.target.value === "name"){
+      console.log(e.target.value)
+      const alphabetical = pokeData.sort((a, b) =>{
+        let fla = a.name.toLowerCase()
+        let flb = b.name.toLowerCase()
+
+        if (fla < flb){
+          return -1
+        } else if (fla > flb){
+          return 1
+        } else{
+          return 0
+        }
+      })
+      setPokeData([...alphabetical])
+    }
+  }
+
+  if(pokeData.length === 0){
+    return <p>Loading...</p>
+  }
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm addPoke={addPoke}/>
       <br />
-      <Search />
+      <Search search={search} searchInput={searchInput}/>
       <br />
-      <PokemonCollection />
+      <label>Sort By: </label>
+      <PokemonCollection displayData={displayData} sortBy={sortBy}/>
     </Container>
   );
 }
